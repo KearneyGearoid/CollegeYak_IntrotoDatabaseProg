cabd8b83440881b34e93e7e71671f1e050235757
Gearoid Kearney <gearoid.kearney@me.com>
2016-11-22 11:34:54 +0000

    Start of Intro to Database Programming Project
    Two Windows
    - MainWindow
    - Dashboard

---
+++ CollegeYak/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CollegeYak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= TextBox_GotFocus;
+        }
+
+        private void btn_Login_Click(object sender, RoutedEventArgs e)
+        {
+            Dashboard dash = new Dashboard();
+            dash.Show();
+            this.Hide();
+        }
+    }
+}
